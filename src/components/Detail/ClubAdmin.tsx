e2a21218f9f3bf4900d2dca4ee5db0eb2c4d7077
jagged91 <>
2022-05-03 17:38:12 -0400

    WIP

--- src/components/Detail/ClubAdmin.tsx
+++ src/components/Detail/ClubAdmin.tsx
@@ -12,7 +12,7 @@ import {
 } from '@mantine/core'
 import { useRouter } from 'next/router'
 import React, { useState } from 'react'
-import { Edit, Settings, Lock, ArrowLeft } from 'tabler-icons-react'
+import { Edit, Settings, Lock, ArrowLeft, Plus } from 'tabler-icons-react'
 
 const useStyles = createStyles(theme => ({
 	header: {
@@ -84,15 +84,27 @@ const useStyles = createStyles(theme => ({
 		color: 'rgba(45, 28, 28, 0.3)',
 		cursor: 'pointer'
 	},
+	// Membership tab
 	membershipText: { fontSize: 20, marginBottom: 8, lineHeight: 2 },
-
 	membershipSelector: {
 		padding: 4,
 		borderRadius: 8,
 		fontWeight: 'bold',
 		backgroundColor: 'rgba(255, 102, 81, 0.1)',
-		color: 'rgba(255, 102, 81, 1)'
+		color: 'rgba(255, 102, 81, 1)',
+		cursor: 'pointer'
 	},
+	addRequirementButton: {
+		backgroundColor: 'white',
+		color: 'rgba(255, 102, 81, 1)',
+		border: '1px dashed rgba(255, 102, 81, 1)',
+		borderRadius: 24,
+		'&:hover': {
+			backgroundColor: 'rgba(255, 102, 81, 0.05)'
+		},
+		marginBottom: 8
+	},
+	// Admins tab
 	clubAdminsPrompt: {
 		fontSize: 18,
 		marginBottom: 16,
@@ -120,6 +132,22 @@ const useStyles = createStyles(theme => ({
 	primaryAdminChipContents: {
 		display: 'flex',
 		alignItems: 'center'
+	},
+	// Integrations tab
+	clubIntegrationsHeader: {
+		fontSize: 18,
+		marginBottom: 16,
+		fontWeight: '600',
+		marginTop: 36,
+		color: 'rgba(0, 0, 0, 0.6)'
+	},
+	clubIntegrationItem: {
+		display: 'flex',
+		alignItems: 'center',
+		fontWeight: '600',
+		marginBottom: 8,
+		cursor: 'pointer',
+		width: '100px'
 	}
 }))
 
@@ -130,22 +158,15 @@ enum Tab {
 }
 
 export const ClubAdminComponent: React.FC = () => {
+	// General properties / tab management
 	const { classes } = useStyles()
 	const router = useRouter()
 	const [isLoading, setIsLoading] = useState(true)
 	const [currentTab, setCurrentTab] = useState<Tab>(Tab.Membership)
 
-	// Membership settings
-	const [whoCanJoin, setWhoCanJoin] = useState('anyone')
-	const [tokenQuantity, setTokenQuantity] = useState('0')
-	const [tokenSymbol, setTokenSymbol] = useState('ETH')
-	const [mintingStart, setMintingStart] = useState('now')
-	const [mintingEnd, setMintingEnd] = useState('never')
-
-	// Club admins
-	const [primaryClubAdmin, setPrimaryClubAdmin] = useState('gadsby.eth')
-	const [clubAdminsString, setClubAdminsString] = useState('')
-	const [clubAdmins, setClubAdmins] = useState<string[]>([])
+	const navigateToClubDetail = () => {
+		router.push({ pathname: '/club/clubname' })
+	}
 
 	const switchToMembership = () => {
 		setCurrentTab(Tab.Membership)
@@ -158,6 +179,76 @@ export const ClubAdminComponent: React.FC = () => {
 	const switchToIntegrations = () => {
 		setCurrentTab(Tab.Integrations)
 	}
+
+	// Membership
+	// TODO: hook up to data
+	//const [whoCanJoin, setWhoCanJoin] = useState('anyone')
+	// const [tokenLimit, setTokenLimit] = useState('unlimited')
+	// const [tokenQuantity, setTokenQuantity] = useState('0')
+	// const [tokenSymbol, setTokenSymbol] = useState('ETH')
+	// const [mintingStart, setMintingStart] = useState('now')
+	// const [mintingEnd, setMintingEnd] = useState('never')
+
+	const addAdditionalRequirement = () => {
+		// TODO
+	}
+
+	const [membershipReqTypeModalOpened, setMembershipReqTypeModalOpened] =
+		useState(false)
+	const chooseMembershipReqType = () => {
+		// e.g. anyone, approved applicants
+		setMembershipReqTypeModalOpened(true)
+	}
+
+	const [tokenHolderReqsModalOpened, setTokenHolderReqsModalOpened] =
+		useState(false)
+	const chooseTokenHolderReqs = () => {
+		// e.g members must hold 1 MATIC
+		// e.g. chain, token, quantity
+		setTokenHolderReqsModalOpened(true)
+	}
+
+	const [mintingDatesModalOpened, setMintingDatesModalOpened] = useState(false)
+	const chooseMintingDates = () => {
+		// e.g. now or later (w/ calendar)
+		setMintingDatesModalOpened(true)
+	}
+
+	const [
+		addtionalMembershipReqAndOrModalOpened,
+		setAddtionalMembershipReqAndOrModalOpened
+	] = useState(false)
+	const chooseAdditionalMembershipReqAndOr = () => {
+		// e.g in addition vs alternatively
+		setAddtionalMembershipReqAndOrModalOpened(true)
+	}
+
+	const [additionalReqTypeModalOpened, setAdditionalReqTypeModalOpened] =
+		useState(false)
+	const chooseAdditionalMembershipReqType = () => {
+		// e.g. choose an application, hold a token, hold an NFT
+		setAdditionalReqTypeModalOpened(true)
+	}
+	const [membershipMintCostModalOpened, setMembershipMintCostModalOpened] =
+		useState(false)
+	const chooseMembershipMintCost = () => {
+		// e.g. our tokens costs X to mint
+		setMembershipMintCostModalOpened(true)
+	}
+
+	const [
+		membershipMintQuantityModalOpened,
+		setMembershipMintQuantityModalOpened
+	] = useState(false)
+	const chooseMembershipMintQuantity = () => {
+		// e.g. there are unlimited tokens available
+		setMembershipMintQuantityModalOpened(true)
+	}
+
+	// Club admins
+	const [primaryClubAdmin, setPrimaryClubAdmin] = useState('gadsby.eth')
+	const [clubAdminsString, setClubAdminsString] = useState('')
+	const [clubAdmins, setClubAdmins] = useState<string[]>([])
 
 	const parseClubAdmins = (rawString: string) => {
 		setClubAdminsString(rawString)
@@ -172,9 +263,17 @@ export const ClubAdminComponent: React.FC = () => {
 		setClubAdmins(finalList)
 	}
 
-	const navigateToClubDetail = () => {
-		router.push({ pathname: '/club/clubname' })
-	}
+	// Integrations
+
+	const handleTwitterIntegration = () => {}
+	const handleDiscordIntegration = () => {}
+	const handleGuildIntegration = () => {}
+	const handleSliksafeIntegration = () => {}
+	const handleTellieIntegration = () => {}
+	const handleClarityIntegration = () => {}
+	const handleGnosisIntegration = () => {}
+	const handleMycoIntegration = () => {}
+	const handleOrcaIntegration = () => {}
 
 	return (
 		<>
@@ -245,21 +344,42 @@ export const ClubAdminComponent: React.FC = () => {
 
 						<Text className={classes.membershipText}>
 							This club is open for{' '}
-							<span className={classes.membershipSelector}>{whoCanJoin}</span>{' '}
+							<a onClick={chooseMembershipReqType}>
+								<span className={classes.membershipSelector}>anyone</span>
+							</a>{' '}
 							to join.
+						</Text>
+						<Button
+							onClick={addAdditionalRequirement}
+							className={classes.addRequirementButton}
+							size={'md'}
+							leftIcon={<Plus size={14} />}
+						>
+							Add another requirement
+						</Button>
+						<Text className={classes.membershipText}>
+							<a onClick={chooseAdditionalMembershipReqAndOr}>
+								<span className={classes.membershipSelector}>In addition</span>
+							</a>
+							, members must{' '}
+							<a onClick={chooseAdditionalMembershipReqType}>
+								<span className={classes.membershipSelector}>...</span>
+							</a>
+							.
 						</Text>
 						<Text className={classes.membershipText}>
 							Our membership token costs{' '}
-							<span className={classes.membershipSelector}>
-								{tokenQuantity} {tokenSymbol}
-							</span>{' '}
-							to mint.
+							<a onClick={chooseMembershipMintQuantity}>
+								<span className={classes.membershipSelector}>1 ETH</span>
+							</a>{' '}
+							to mint. There are
+							<span className={classes.membershipSelector}>unlimited</span>{' '}
+							tokens available.
 						</Text>
 						<Text className={classes.membershipText}>
 							Minting starts{' '}
-							<span className={classes.membershipSelector}>{mintingStart}</span>{' '}
-							and ends{' '}
-							<span className={classes.membershipSelector}>{mintingEnd}</span>.
+							<span className={classes.membershipSelector}>now</span> and ends{' '}
+							<span className={classes.membershipSelector}>never</span>.
 						</Text>
 					</div>
 				)}
@@ -299,7 +419,126 @@ export const ClubAdminComponent: React.FC = () => {
 					</div>
 				)}
 
-				<Button className={classes.buttonSaveChanges}>Save Changes</Button>
+				{currentTab === Tab.Integrations && (
+					<div>
+						<Text className={classes.clubIntegrationsHeader}>Social</Text>
+						<a onClick={handleTwitterIntegration}>
+							<div className={classes.clubIntegrationItem}>
+								<Image
+									src="/integration-twitter.png"
+									width={16}
+									height={16}
+									fit={'contain'}
+								/>
+								<Space w={'xs'} />
+								<Text>Twitter</Text>
+							</div>
+						</a>
+						<a onClick={handleDiscordIntegration}>
+							<div className={classes.clubIntegrationItem}>
+								<Image
+									src="/integration-discord.png"
+									width={16}
+									height={16}
+									fit={'contain'}
+								/>
+								<Space w={'xs'} />
+								<Text>Discord</Text>
+							</div>
+						</a>
+						<Text className={classes.clubIntegrationsHeader}>Token Gating</Text>
+						<a onClick={handleGuildIntegration}>
+							<div className={classes.clubIntegrationItem}>
+								<Image
+									src="/integration-guild.png"
+									width={16}
+									height={16}
+									fit={'contain'}
+								/>
+								<Space w={'xs'} />
+								<Text>Guild</Text>
+							</div>
+						</a>
+						<a onClick={handleSliksafeIntegration}>
+							<div className={classes.clubIntegrationItem}>
+								<Image
+									src="/integration-sliksafe.png"
+									width={16}
+									height={16}
+									fit={'contain'}
+								/>
+								<Space w={'xs'} />
+								<Text>Sliksafe</Text>
+							</div>
+						</a>
+						<a onClick={handleTellieIntegration}>
+							<div className={classes.clubIntegrationItem}>
+								<Image
+									src="/integration-tellie.png"
+									width={16}
+									height={16}
+									fit={'contain'}
+								/>
+								<Space w={'xs'} />
+								<Text>Tellie</Text>
+							</div>
+						</a>
+						<a onClick={handleClarityIntegration}>
+							<div className={classes.clubIntegrationItem}>
+								<Image
+									src="/integration-clarity.png"
+									width={16}
+									height={16}
+									fit={'contain'}
+								/>
+								<Space w={'xs'} />
+								<Text>Clarity</Text>
+							</div>
+						</a>
+						<Text className={classes.clubIntegrationsHeader}>DAO Tools</Text>
+						<a onClick={handleGnosisIntegration}>
+							<div className={classes.clubIntegrationItem}>
+								<Image
+									src="/integration-gnosis.png"
+									width={16}
+									height={16}
+									fit={'contain'}
+								/>
+								<Space w={'xs'} />
+								<Text>Gnosis</Text>
+							</div>
+						</a>
+						<a onClick={handleMycoIntegration}>
+							<div className={classes.clubIntegrationItem}>
+								<Image
+									src="/integration-myco.png"
+									width={16}
+									height={16}
+									fit={'contain'}
+								/>
+								<Space w={'xs'} />
+								<Text>Myco</Text>
+							</div>
+						</a>
+						<a onClick={handleOrcaIntegration}>
+							<div className={classes.clubIntegrationItem}>
+								<Image
+									src="/integration-orca.png"
+									width={16}
+									height={16}
+									fit={'contain'}
+								/>
+								<Space w={'xs'} />
+								<Text>Orca</Text>
+							</div>
+						</a>
+						<Space h="xl" />
+					</div>
+				)}
+				{currentTab === Tab.Membership ||
+					(currentTab == Tab.Admins && (
+						<Button className={classes.buttonSaveChanges}>Save Changes</Button>
+					))}
 			</Container>
 		</>
 	)
