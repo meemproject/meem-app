14cd66da63f2000e8f9a337884bea8821be1aa9a
jagged91 <>
2022-06-13 11:52:10 -0400

    Show my clubs as a grid instead

--- src/components/MyClubs/MyClubs.tsx
+++ src/components/MyClubs/MyClubs.tsx
@@ -13,7 +13,8 @@ import {
 	Button,
 	Space,
 	Center,
-	Loader
+	Loader,
+	Grid
 } from '@mantine/core'
 import { useWallet } from '@meemproject/react'
 import { useRouter } from 'next/router'
@@ -191,25 +192,37 @@ export const MyClubsComponent: React.FC = () => {
 				)}
 				{clubs.length > 0 && !loading && (
 					<>
-						{clubs.map(club => (
-							<div
-								key={club.address}
-								className={classes.clubItem}
-								onClick={() => {
-									navigateToClub(club.slug!)
-								}}
-							>
-								<Image
-									className={classes.clubLogoImage}
-									src={club.image!}
-									width={40}
-									height={40}
-									fit={'contain'}
-								/>
-								<Space w="xs" />
-								<Text>{club.name!}</Text>
-							</div>
-						))}
+						<Grid>
+							{clubs.map(club => (
+								<Grid.Col
+									xs={6}
+									sm={4}
+									md={4}
+									lg={4}
+									xl={4}
+									key={club.address}
+								>
+									<div
+										key={club.address}
+										className={classes.clubItem}
+										onClick={() => {
+											navigateToClub(club.slug!)
+										}}
+									>
+										<Image
+											className={classes.clubLogoImage}
+											src={club.image!}
+											width={40}
+											height={40}
+											fit={'contain'}
+										/>
+										<Space w="xs" />
+										<Text>{club.name!}</Text>
+									</div>
+								</Grid.Col>
+							))}
+						</Grid>
+
 						<Space h={60} />
 					</>
 				)}
