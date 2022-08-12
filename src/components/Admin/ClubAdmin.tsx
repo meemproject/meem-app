6bce29ae0f7807d4cc5051b0eb9a2d5462d0e43c
Ken Goldfarb <hello@kengoldfarb.com>
2022-08-12 09:37:29 -0400

    lint

--- src/components/Admin/ClubAdmin.tsx
+++ src/components/Admin/ClubAdmin.tsx
@@ -14,7 +14,6 @@ import {
 import { showNotification } from '@mantine/notifications'
 import { makeFetcher, MeemAPI } from '@meemproject/api'
 import { LoginState, useWallet } from '@meemproject/react'
-import Cookies from 'js-cookie'
 import { useRouter } from 'next/router'
 import React, { useEffect, useState } from 'react'
 import { ArrowLeft, Check } from 'tabler-icons-react'
