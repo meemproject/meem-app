3508843192b95d696df0a5f4fb0a4bccb76cebf9
James Gadsby <james.gadsby@gmail.com>
2022-08-16 12:01:06 -0400

    WIP - profile picture

--- src/components/Profile/Tabs/Identity/ManageIdentity.tsx
+++ src/components/Profile/Tabs/Identity/ManageIdentity.tsx
@@ -1,18 +1,24 @@
+import log from '@kengoldfarb/log'
 import {
 	createStyles,
-	Container,
 	Text,
-	Image,
 	Button,
 	Space,
-	Center,
+	Divider,
+	Image,
+	Modal,
 	Loader,
-	Grid
+	TextInput
 } from '@mantine/core'
 import { useWallet } from '@meemproject/react'
+import { base64StringToBlob } from 'blob-util'
+import html2canvas from 'html2canvas'
+import dynamic from 'next/dynamic'
 import { useRouter } from 'next/router'
-import React, { useState } from 'react'
-import { ArrowLeft } from 'tabler-icons-react'
+import React, { useEffect, useState } from 'react'
+import Resizer from 'react-image-file-resizer'
+import { Upload } from 'tabler-icons-react'
+import { useFilePicker } from 'use-file-picker'
 import { Identity } from '../../../../model/identity/identity'
 import { ProfileLinkDiscordModal } from './ProfileLinkDiscordModal'
 import { ProfileLinkEmailModal } from './ProfileLinkEmailModal'
@@ -58,13 +64,22 @@ const useStyles = createStyles(theme => ({
 			fontSize: 20
 		}
 	},
-	buttonCreate: {
+	buttonSaveChanges: {
 		backgroundColor: 'black',
 		'&:hover': {
 			backgroundColor: theme.colors.gray[8]
 		},
 		borderRadius: 24,
 		marginRight: 32
+	},
+	buttonUpload: {
+		borderRadius: 24,
+		color: 'black',
+		borderColor: 'black',
+		backgroundColor: 'white',
+		'&:hover': {
+			backgroundColor: theme.colors.gray[0]
+		}
 	},
 	createClubLink: {
 		marginTop: 24,
@@ -89,15 +104,50 @@ const useStyles = createStyles(theme => ({
 	clubLogoImage: {
 		imageRendering: 'pixelated'
 	},
+	clubLogoImageContainer: {
+		marginTop: 24,
+		width: 108,
+		height: 100,
+		position: 'relative'
+	},
+	clubLogoDeleteButton: {
+		position: 'absolute',
+		top: '-12px',
+		right: '-105px',
+		cursor: 'pointer'
+	},
+	uploadOptions: { display: 'flex' },
+	emojiCanvas: {
+		position: 'absolute',
+		top: 40,
+		left: 0,
+		marginTop: -12,
+		marginBottom: -12,
+		lineHeight: 1,
+		fontSize: 24,
+		zIndex: -1000
+	},
 	clubNameEllipsis: {
 		textOverflow: 'ellipsis',
 		msTextOverflow: 'ellipsis',
 		whiteSpace: 'nowrap',
 		overflow: 'hidden'
 	},
-
+	identitySectionTitle: {
+		fontSize: 18,
+		marginBottom: 16,
+		fontWeight: 600,
+		[`@media (max-width: ${theme.breakpoints.md}px)`]: {
+			fontSize: 16,
+			marginBottom: 8
+		}
+	},
 	myClubsPrompt: { fontSize: 18, marginBottom: 16 }
 }))
+
+const EmojiPicker = dynamic(() => import('emoji-picker-react'), {
+	ssr: false
+})
 
 interface IProps {
 	identity: Identity
@@ -108,6 +158,65 @@ export const ManageIdentityComponent: React.FC<IProps> = ({ identity }) => {
 	const router = useRouter()
 	const wallet = useWallet()
 
+	// Mutable identity data
+	const [displayName, setDisplayName] = useState(identity.displayName ?? '')
+	const [profilePicture, setProfilePicture] = useState(
+		identity.profilePic ?? ''
+	)
+
+	const [chosenEmoji, setChosenEmoji] = useState<any>(null)
+
+	// Club logo
+	const [
+		openFileSelector,
+		{ filesContent: clubLogo, loading: isLoadingImage }
+	] = useFilePicker({
+		readAs: 'DataURL',
+		accept: 'image/*',
+		limitFilesConfig: { max: 1 },
+		multiple: false,
+		maxFileSize: 10
+	})
+
+	const resizeFile = (file: any) =>
+		new Promise(resolve => {
+			Resizer.imageFileResizer(
+				file,
+				256,
+				256,
+				'PNG',
+				100,
+				0,
+				uri => {
+					resolve(uri)
+				},
+				'base64'
+			)
+		})
+
+	useEffect(() => {
+		const createResizedFile = async () => {
+			const clubLogoBlob = base64StringToBlob(
+				clubLogo[0].content.split(',')[1],
+				'image/png'
+			)
+			const file = await resizeFile(clubLogoBlob)
+			setProfilePicture(file as string)
+		}
+		if (clubLogo.length > 0) {
+			log.debug('Found an image...')
+			log.debug(clubLogo[0].content)
+
+			createResizedFile()
+		} else {
+			// log.debug('no current club image')
+		}
+	}, [clubLogo])
+
+	const deleteImage = () => {
+		setProfilePicture('')
+	}
+
 	// Modal visibility
 	const [isLinkedAccountModalOpen, setIsLinkedAccountModalOped] =
 		useState(false)
@@ -116,10 +225,109 @@ export const ManageIdentityComponent: React.FC<IProps> = ({ identity }) => {
 	const [isTwitterModalOpen, setIsTwitterModalOpen] = useState(false)
 	const [isEmailModalOpen, setIsEmailModalOpen] = useState(false)
 
+	const [isEmojiPickerOpen, setIsEmojiPickerOpen] = useState(false)
+	const openEmojiPicker = () => {
+		setIsEmojiPickerOpen(true)
+	}
+
+	function timeout(delay: number) {
+		return new Promise(res => setTimeout(res, delay))
+	}
+
+	const onEmojiClick = async (event: any, emojiObject: any) => {
+		setChosenEmoji(emojiObject)
+		setIsEmojiPickerOpen(false)
+		await timeout(100)
+		const element = document.querySelector('#emojiCanvas')
+		if (element) {
+			log.debug('found emojiCanvas')
+
+			const canvas = await html2canvas(element as HTMLElement)
+			const image = canvas.toDataURL('image/png', 1.0)
+			const clubLogoBlob = base64StringToBlob(
+				image.split(',')[1],
+				'image/png'
+			)
+			const file = await resizeFile(clubLogoBlob)
+			setProfilePicture(file as string)
+		} else {
+			log.debug('no emojiCanvas found')
+		}
+	}
+
+	const [isSavingChanges, setIsSavingChanges] = useState(false)
+
+	const saveChanges = async () => {
+		setIsSavingChanges(true)
+	}
+
 	return (
 		<>
 			<Space h={30} />
-			<Text>Manage identity here</Text>
+			<Text className={classes.identitySectionTitle}>
+				Profile Picture
+			</Text>
+
+			{profilePicture.length === 0 && !isLoadingImage && (
+				<div className={classes.uploadOptions}>
+					<Button
+						leftIcon={<Upload size={14} />}
+						className={classes.buttonUpload}
+						onClick={() => openFileSelector()}
+					>
+						Upload
+					</Button>
+					<Space w={'xs'} />
+					<Button
+						leftIcon={<Text>😈</Text>}
+						className={classes.buttonUpload}
+						onClick={() => openEmojiPicker()}
+					>
+						Choose emoji
+					</Button>
+				</div>
+			)}
+			{isLoadingImage && <Loader color="red" variant="bars" />}
+			{!isLoadingImage && profilePicture.length > 0 && (
+				<div className={classes.clubLogoImageContainer}>
+					<Image
+						className={classes.clubLogoImage}
+						src={profilePicture}
+						width={200}
+						height={200}
+						fit={'cover'}
+					/>
+					<a onClick={deleteImage}>
+						<Image
+							className={classes.clubLogoDeleteButton}
+							src="/delete.png"
+							width={24}
+							height={24}
+						/>
+					</a>
+				</div>
+			)}
+			<Space h={profilePicture.length > 0 ? 148 : 32} />
+
+			<Text className={classes.identitySectionTitle}>Display Name</Text>
+			<TextInput
+				radius="lg"
+				size="md"
+				value={displayName}
+				onChange={event => setDisplayName(event.currentTarget.value)}
+			/>
+			<Space h={'xl'} />
+			<Divider />
+			<Text className={classes.identitySectionTitle}>
+				Verify Accounts
+			</Text>
+			<Button
+				className={classes.buttonSaveChanges}
+				loading={isSavingChanges}
+				onClick={saveChanges}
+			>
+				Save Changes
+			</Button>
 			<ProfileLinkTwitterModal
 				identity={identity}
 				isOpened={isTwitterModalOpen}
@@ -150,6 +358,21 @@ export const ManageIdentityComponent: React.FC<IProps> = ({ identity }) => {
 					setIsDiscordModalOpen(false)
 				}}
 			/>
+			<div id="emojiCanvas" className={classes.emojiCanvas}>
+				{chosenEmoji && <>{chosenEmoji.emoji}</>}
+			</div>
+			<Modal
+				withCloseButton={false}
+				padding={8}
+				overlayBlur={8}
+				size={296}
+				opened={isEmojiPickerOpen}
+				onClose={() => {
+					setIsEmojiPickerOpen(false)
+				}}
+			>
+				<EmojiPicker onEmojiClick={onEmojiClick} />
+			</Modal>
 		</>
 	)
 }
