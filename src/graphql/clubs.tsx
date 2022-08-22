36dd55242c0d15bf6439a0ef8f879cfeaf2caff0
James Gadsby <james.gadsby@gmail.com>
2022-08-22 18:13:03 -0400

    MeemID subscription

--- src/graphql/clubs.tsx
+++ src/graphql/clubs.tsx
@@ -312,3 +312,22 @@ export const GET_BUNDLE_BY_ID = gql`
 		}
 	}
 `
+
+export const MEEM_ID_SUBSCRIPTION = gql`
+	subscription MeemIdSubscription($walletAddress: String) {
+		MeemIdentities(
+			where: { Wallet: { address: { _ilike: $walletAddress } } }
+		) {
+			updatedAt
+			profilePicUrl
+			id
+			displayName
+			deletedAt
+			createdAt
+			Wallet {
+				address
+				ens
+			}
+		}
+	}
+`
