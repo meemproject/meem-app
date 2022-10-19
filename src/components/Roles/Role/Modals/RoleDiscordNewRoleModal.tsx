0e7ade66dbca10ff1cc1a5592cb5cda4714dfee2
James Gadsby <james.gadsby@gmail.com>
2022-10-19 09:31:12 -0400

    Fix 'saving changes' state on new role modal

--- src/components/Roles/Role/Modals/RoleDiscordNewRoleModal.tsx
+++ src/components/Roles/Role/Modals/RoleDiscordNewRoleModal.tsx
@@ -140,117 +140,129 @@ export const RoleDiscordNewRoleModal: React.FC<IProps> = ({
 				{isOpened && isSavingChanges && (
 					<>
 						<Center>
-							<Loader />
+							<div>
+								<Loader color={'red'} variant={'oval'} />
+								<Space h={24} />
+							</div>
 						</Center>
 					</>
 				)}
-				{server && server.guildData.channels.length === 0 && (
-					<>
-						<Center>
-							<Text>
-								Sorry - unable to load Discord channels. Try
-								again later, or make sure the server you
-								selected has at least one channel.
-							</Text>
-							<Space h={16} />
-							<Button
-								onClick={() => {
-									onModalClosed()
+				{server &&
+					!isSavingChanges &&
+					server.guildData.channels.length === 0 && (
+						<>
+							<Center>
+								<Text>
+									Sorry - unable to load Discord channels. Try
+									again later, or make sure the server you
+									selected has at least one channel.
+								</Text>
+								<Space h={16} />
+								<Button
+									onClick={() => {
+										onModalClosed()
+									}}
+									className={styles.buttonGrey}
+								>
+									Close
+								</Button>
+							</Center>
+						</>
+					)}
+				{server &&
+					!isSavingChanges &&
+					server.guildData.channels.length > 0 && (
+						<>
+							<Space h={8} />
+							<div className={styles.row}>
+								<Text className={styles.tSectionTitleSmall}>
+									DISCORD ROLE NAME
+								</Text>
+								<Space w={2} />
+								<Text color={'red'}>*</Text>
+							</div>
+							<Space h={12} />
+							<TextInput
+								size={'lg'}
+								radius={20}
+								placeholder={'Enter Role Name'}
+								classNames={{
+									input: styles.fTextField
 								}}
-								className={styles.buttonGrey}
-							>
-								Close
-							</Button>
-						</Center>
-					</>
-				)}
-				{server && server.guildData.channels.length > 0 && (
-					<>
-						<Space h={8} />
-						<div className={styles.row}>
-							<Text className={styles.tSectionTitleSmall}>
-								DISCORD ROLE NAME
+								value={discordRoleName}
+								onChange={event => {
+									if (event) {
+										setDiscordRoleName(event.target.value)
+									}
+								}}
+							/>
+							<Space h={32} />
+
+							<Text className={styles.tBold}>
+								Channels to Gate
 							</Text>
-							<Space w={2} />
-							<Text color={'red'}>*</Text>
-						</div>
-						<Space h={12} />
-						<TextInput
-							size={'lg'}
-							radius={20}
-							placeholder={'Enter Role Name'}
-							classNames={{
-								input: styles.fTextField
-							}}
-							value={discordRoleName}
-							onChange={event => {
-								if (event) {
-									setDiscordRoleName(event.target.value)
-								}
-							}}
-						/>
-						<Space h={32} />
+							<Space h={8} />
 
-						<Text className={styles.tBold}>Channels to Gate</Text>
-						<Space h={8} />
-
-						<Checkbox.Group
-							orientation="vertical"
-							spacing={'xs'}
-							value={selectedDiscordCategories}
-							onChange={categories => {
-								// Logic to select / de-select all the channels in a category when a category is selected
-								setSelectedDiscordCategories(categories)
-								const channels: string[] = []
-								categories.forEach(category => {
-									let matchingCategory: any
-									server.guildData.categories.forEach(
-										originalCategory => {
-											if (
-												category === originalCategory.id
-											) {
-												matchingCategory =
-													originalCategory
+							<Checkbox.Group
+								orientation="vertical"
+								spacing={'xs'}
+								value={selectedDiscordCategories}
+								onChange={categories => {
+									// Logic to select / de-select all the channels in a category when a category is selected
+									setSelectedDiscordCategories(categories)
+									const channels: string[] = []
+									categories.forEach(category => {
+										let matchingCategory: any
+										server.guildData.categories.forEach(
+											originalCategory => {
+												if (
+													category ===
+													originalCategory.id
+												) {
+													matchingCategory =
+														originalCategory
+												}
 											}
-										}
-									)
-									matchingCategory.channels.forEach(
-										(channel: any) => {
-											channels.push(channel.id)
-										}
-									)
-								})
-								setSelectedDiscordChannels(channels)
-							}}
-						>
-							{server.guildData.categories.map(category => (
-								<div key={category.id}>
-									<Checkbox
-										value={category.id}
-										label={category.name}
-									/>
-									<div style={{ marginLeft: 32 }}>
-										<Checkbox.Group
-											orientation="vertical"
-											spacing={'xs'}
-											value={selectedDiscordChannels}
-											onChange={
-												setSelectedDiscordChannels
+										)
+										matchingCategory.channels.forEach(
+											(channel: any) => {
+												channels.push(channel.id)
 											}
-										>
-											{category.channels.map(channel => (
-												<Checkbox
-													key={channel.id}
-													value={channel.id}
-													label={channel.name}
-												/>
-											))}
-										</Checkbox.Group>
+										)
+									})
+									setSelectedDiscordChannels(channels)
+								}}
+							>
+								{server.guildData.categories.map(category => (
+									<div key={category.id}>
+										<Checkbox
+											value={category.id}
+											label={category.name}
+										/>
+										<div style={{ marginLeft: 32 }}>
+											<Checkbox.Group
+												orientation="vertical"
+												spacing={'xs'}
+												value={selectedDiscordChannels}
+												onChange={
+													setSelectedDiscordChannels
+												}
+											>
+												{category.channels.map(
+													channel => (
+														<Checkbox
+															key={channel.id}
+															value={channel.id}
+															label={channel.name}
+														/>
+													)
+												)}
+											</Checkbox.Group>
+										</div>
 									</div>
-								</div>
-							))}
-						</Checkbox.Group>
-						{/* <Checkbox.Group
+								))}
+							</Checkbox.Group>
+							{/* <Checkbox.Group
 							orientation="vertical"
 							spacing={'xs'}
 							value={selectedDiscordChannels}
@@ -265,36 +277,36 @@ export const RoleDiscordNewRoleModal: React.FC<IProps> = ({
 							))}
 						</Checkbox.Group> */}
 
-						<Space h={32} />
+							<Space h={32} />
 
-						<div className={styles.row}>
-							<Button
-								disabled={isSavingChanges}
-								loading={isSavingChanges}
-								className={styles.buttonBlack}
-								onClick={async () => {
-									saveChanges()
-								}}
-							>
-								{'Save Changes'}
-							</Button>
-							<Space w={8} />
-							{!isSavingChanges && (
+							<div className={styles.row}>
 								<Button
-									onClick={() => {
-										setSelectedDiscordChannels([])
-										setSelectedDiscordCategories([])
-										setDiscordRoleName('')
-										onModalClosed()
+									disabled={isSavingChanges}
+									loading={isSavingChanges}
+									className={styles.buttonBlack}
+									onClick={async () => {
+										saveChanges()
 									}}
-									className={styles.buttonGrey}
 								>
-									Cancel
+									{'Save Changes'}
 								</Button>
-							)}
-						</div>
-					</>
-				)}
+								<Space w={8} />
+								{!isSavingChanges && (
+									<Button
+										onClick={() => {
+											setSelectedDiscordChannels([])
+											setSelectedDiscordCategories([])
+											setDiscordRoleName('')
+											onModalClosed()
+										}}
+										className={styles.buttonGrey}
+									>
+										Cancel
+									</Button>
+								)}
+							</div>
+						</>
+					)}
 			</Modal>
 		</>
 	)
