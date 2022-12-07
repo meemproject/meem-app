50e9392963d10c48929b268d86bd62d78faa0d30
James Gadsby <james.gadsby@gmail.com>
2022-12-07 16:34:45 -0500

    More cleanup

--- src/pages/createtools.tsx
+++
@@ -1,9 +0,0 @@
-import type { NextPage } from 'next'
-import React from 'react'
-// import { CreatePage } from '../components/CreatePage'
-
-const Home: NextPage = () => {
-	return <div>{/* <CreatePage /> */}</div>
-}
-
-export default Home
