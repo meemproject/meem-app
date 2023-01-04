d4ee1a14e714ced10787f957ba3430e6c3448636
James Gadsby <james.gadsby@gmail.com>
2023-01-04 14:07:47 -0500

    Prevent duplicate reinitialize calls

--- src/components/Admin/AgreementAdminChangesModal.tsx
+++ src/components/Admin/AgreementAdminChangesModal.tsx
@@ -92,145 +92,158 @@ export const AgreementAdminChangesModal: React.FC<IProps> = ({
 				return
 			}
 
-			setIsSavingChanges(true)
+			if (!isSavingChanges) {
+				setIsSavingChanges(true)
 
-			log.debug(`reinitialize...`)
+				log.debug(`reinitialize...`)
 
-			try {
-				// const agreementSymbol = (agreement.name ?? '').split(' ')[0].toUpperCase()
+				try {
+					// const agreementSymbol = (agreement.name ?? '').split(' ')[0].toUpperCase()
 
-				const applicationInstructions: string[] = []
-				if (agreement.membershipSettings) {
-					agreement.membershipSettings.requirements.forEach(
-						requirement => {
-							if (
-								requirement.applicationInstructions &&
-								requirement.applicationInstructions?.length > 0
-							) {
-								applicationInstructions.push(
+					const applicationInstructions: string[] = []
+					if (agreement.membershipSettings) {
+						agreement.membershipSettings.requirements.forEach(
+							requirement => {
+								if (
+									requirement.applicationInstructions &&
 									requirement.applicationInstructions
-								)
+										?.length > 0
+								) {
+									applicationInstructions.push(
+										requirement.applicationInstructions
+									)
+								}
 							}
+						)
+					}
+
+					let membershipStartUnix = -1
+					let membershipEndUnix = -1
+					if (agreement.membershipSettings) {
+						if (agreement.membershipSettings.membershipStartDate) {
+							membershipStartUnix = Math.floor(
+								new Date(
+									agreement.membershipSettings.membershipStartDate
+								).getTime() / 1000
+							)
+							log.debug(membershipStartUnix)
 						}
-					)
-				}
-
-				let membershipStartUnix = -1
-				let membershipEndUnix = -1
-				if (agreement.membershipSettings) {
-					if (agreement.membershipSettings.membershipStartDate) {
-						membershipStartUnix = Math.floor(
-							new Date(
-								agreement.membershipSettings.membershipStartDate
-							).getTime() / 1000
-						)
-						log.debug(membershipStartUnix)
+						if (agreement.membershipSettings.membershipEndDate) {
+							membershipEndUnix = Math.floor(
+								new Date(
+									agreement.membershipSettings.membershipEndDate
+								).getTime() / 1000
+							)
+							log.debug(membershipEndUnix)
+						}
 					}
-					if (agreement.membershipSettings.membershipEndDate) {
-						membershipEndUnix = Math.floor(
-							new Date(
-								agreement.membershipSettings.membershipEndDate
-							).getTime() / 1000
-						)
-						log.debug(membershipEndUnix)
-					}
-				}
 
-				let mintPermissions: MeemAPI.IMeemPermission[] = []
-				if (agreement && agreement.membershipSettings) {
-					mintPermissions =
-						agreement.membershipSettings.requirements.map(mr => {
-							return MembershipRequirementToMeemPermission({
-								...mr,
-								costEth:
-									agreement.membershipSettings?.costToJoin,
-								mintStartTimestamp: agreement.membershipSettings
-									?.membershipStartDate
-									? agreement.membershipSettings?.membershipStartDate.getTime() /
-									  1000
-									: 0,
-								mintEndTimestamp: agreement.membershipSettings
-									?.membershipEndDate
-									? agreement.membershipSettings?.membershipEndDate.getTime() /
-									  1000
-									: 0
-							})
+					let mintPermissions: MeemAPI.IMeemPermission[] = []
+					if (agreement && agreement.membershipSettings) {
+						mintPermissions =
+							agreement.membershipSettings.requirements.map(
+								mr => {
+									return MembershipRequirementToMeemPermission(
+										{
+											...mr,
+											costEth:
+												agreement.membershipSettings
+													?.costToJoin,
+											mintStartTimestamp: agreement
+												.membershipSettings
+												?.membershipStartDate
+												? agreement.membershipSettings?.membershipStartDate.getTime() /
+												  1000
+												: 0,
+											mintEndTimestamp: agreement
+												.membershipSettings
+												?.membershipEndDate
+												? agreement.membershipSettings?.membershipEndDate.getTime() /
+												  1000
+												: 0
+										}
+									)
+								}
+							)
+					}
+
+					if (!agreement.id) {
+						showNotification({
+							radius: 'lg',
+							title: 'Error saving community settings',
+							message: `Please get in touch!`,
+							color: colorBlue
 						})
-				}
+						closeModal()
+						return
+					}
 
-				if (!agreement.id) {
+					if (mintPermissions.length === 0) {
+						showNotification({
+							radius: 'lg',
+							title: 'Oops!',
+							message: `This community has invalid membership requirements. Please double-check your entries and try again.`,
+							color: colorBlue
+						})
+						closeModal()
+						return
+					}
+
+					const data = {
+						agreementId: agreement.id,
+						metadata: {
+							meem_metadata_type: 'Meem_AgreementContract',
+							meem_metadata_version: '20221116',
+							name: agreement.name,
+							description: agreement.description,
+							image: agreement.image,
+							associations: [],
+							external_url: `https://app.meem.wtf/${agreement.slug}`,
+							application_instructions: applicationInstructions
+						},
+						name: agreement.name ?? '',
+						admins: agreement.adminAddresses,
+						minters: agreement.adminAddresses,
+						maxSupply: !isNaN(
+							agreement.membershipSettings?.membershipQuantity ??
+								0
+						)
+							? `${agreement.membershipSettings?.membershipQuantity}`
+							: '0',
+						mintPermissions,
+						splits:
+							agreement.membershipSettings &&
+							agreement.membershipSettings.membershipFundsAddress
+								.length > 0
+								? [
+										{
+											toAddress:
+												agreement.membershipSettings
+													? agreement
+															.membershipSettings
+															.membershipFundsAddress
+													: wallet.accounts[0],
+											// Amount in basis points 10000 == 100%
+											amount: 10000,
+											lockedBy: MeemAPI.zeroAddress
+										}
+								  ]
+								: []
+					}
+
+					log.debug(data)
+					const { txId } = await sdk.agreement.reInitialize(data)
+
+					log.debug(`Reinitializing agreement w/ txId: ${txId}`)
+				} catch (e) {
+					log.debug(e)
 					showNotification({
 						radius: 'lg',
 						title: 'Error saving community settings',
-						message: `Please get in touch!`,
-						color: colorBlue
+						message: `Please get in touch!`
 					})
 					closeModal()
-					return
 				}
-
-				if (mintPermissions.length === 0) {
-					showNotification({
-						radius: 'lg',
-						title: 'Oops!',
-						message: `This community has invalid membership requirements. Please double-check your entries and try again.`,
-						color: colorBlue
-					})
-					closeModal()
-					return
-				}
-
-				const data = {
-					agreementId: agreement.id,
-					metadata: {
-						meem_metadata_type: 'Meem_AgreementContract',
-						meem_metadata_version: '20221116',
-						name: agreement.name,
-						description: agreement.description,
-						image: agreement.image,
-						associations: [],
-						external_url: `https://app.meem.wtf/${agreement.slug}`,
-						application_instructions: applicationInstructions
-					},
-					name: agreement.name ?? '',
-					admins: agreement.adminAddresses,
-					minters: agreement.adminAddresses,
-					maxSupply: !isNaN(
-						agreement.membershipSettings?.membershipQuantity ?? 0
-					)
-						? `${agreement.membershipSettings?.membershipQuantity}`
-						: '0',
-					mintPermissions,
-					splits:
-						agreement.membershipSettings &&
-						agreement.membershipSettings.membershipFundsAddress
-							.length > 0
-							? [
-									{
-										toAddress: agreement.membershipSettings
-											? agreement.membershipSettings
-													.membershipFundsAddress
-											: wallet.accounts[0],
-										// Amount in basis points 10000 == 100%
-										amount: 10000,
-										lockedBy: MeemAPI.zeroAddress
-									}
-							  ]
-							: []
-				}
-
-				log.debug(data)
-				const { txId } = await sdk.agreement.reInitialize(data)
-
-				log.debug(`Reinitializing agreement w/ txId: ${txId}`)
-			} catch (e) {
-				log.debug(e)
-				showNotification({
-					radius: 'lg',
-					title: 'Error saving community settings',
-					message: `Please get in touch!`
-				})
-				closeModal()
 			}
 		}
 		function compareAgreementData() {
