a77f1929ea1c2e61b4bc5427acd11587cdb53024
James Gadsby <james.gadsby@gmail.com>
2023-01-19 11:16:13 -0500

    Add extension boilerplate

---
+++ src/components/Extensions/EtsyLink/EtsyLinkExtensionSettings.tsx
@@ -0,0 +1,249 @@
+import {
+	Container,
+	Text,
+	Space,
+	Center,
+	Button,
+	Divider,
+	Switch,
+	TextInput
+} from '@mantine/core'
+import { useSDK } from '@meemproject/react'
+import { MeemAPI } from '@meemproject/sdk'
+import React, { useEffect, useState } from 'react'
+import { extensionFromSlug } from '../../../model/agreement/agreements'
+import {
+	showErrorNotification,
+	showSuccessNotification
+} from '../../../utils/notifications'
+import { useAgreement } from '../../AgreementHome/AgreementProvider'
+import { useMeemTheme } from '../../Styles/MeemTheme'
+import { ExtensionBlankSlate, extensionIsReady } from '../ExtensionBlankSlate'
+import { ExtensionPageHeader } from '../ExtensionPageHeader'
+
+export const EtsyLinkExtensionSettings: React.FC = () => {
+	const { classes: meemTheme } = useMeemTheme()
+	const { agreement, isLoadingAgreement } = useAgreement()
+	const agreementExtension = extensionFromSlug('etsy', agreement)
+	const sdk = useSDK()
+
+	const [isSavingChanges, setIsSavingChanges] = useState(false)
+	const [isDisablingExtension, setIsDisablingExtension] = useState(false)
+	const [shouldDisplayInSidebar, setShouldDisplayInSidebar] = useState(true)
+	const [shouldDisplayInFavoriteLinks, setShouldDisplayInFavoriteLinks] =
+		useState(true)
+
+	const [isPrivateExtension, setIsPrivateExtension] = useState(false)
+	const [isExistingDataSetup, setIsExistingDataSetup] = useState(false)
+	const [linkUrl, setLinkUrl] = useState('')
+
+	useEffect(() => {
+		if (
+			!isExistingDataSetup &&
+			agreementExtension &&
+			agreementExtension.AgreementExtensionLinks[0]
+		) {
+			setIsExistingDataSetup(true)
+			setLinkUrl(agreementExtension.AgreementExtensionLinks[0].url)
+			setShouldDisplayInSidebar(
+				agreementExtension.metadata.sidebarVisible
+			)
+			setShouldDisplayInFavoriteLinks(
+				agreementExtension.metadata.favoriteLinksVisible
+			)
+			setIsPrivateExtension(
+				agreementExtension.AgreementExtensionLinks[0].visibility ===
+					MeemAPI.AgreementExtensionVisibility.TokenHolders
+			)
+		}
+	}, [agreementExtension, isExistingDataSetup])
+
+	const saveChanges = async () => {
+		if (
+			linkUrl.length === 0 ||
+			linkUrl.length > 100
+		) {
+			showErrorNotification('Oops!', 'Please enter a valid URL.')
+			return
+		}
+
+		setIsSavingChanges(true)
+		await sdk.sdk.agreementExtension.updateAgreementExtension({
+			agreementId: agreement?.id ?? '',
+			agreementExtensionId: agreementExtension?.id,
+			metadata: {
+				sidebarVisible: shouldDisplayInSidebar,
+				favoriteLinksVisible: shouldDisplayInFavoriteLinks
+			},
+			externalLink: {
+				url: linkUrl,
+				isEnabled: true,
+				visibility: isPrivateExtension
+					? MeemAPI.AgreementExtensionVisibility.TokenHolders
+					: MeemAPI.AgreementExtensionVisibility.Anyone
+			}
+		})
+		showSuccessNotification('Success!', 'This extension has been updated.')
+		setIsSavingChanges(false)
+	}
+
+	const disableExtension = async () => {
+		setIsDisablingExtension(true)
+		setIsDisablingExtension(false)
+	}
+
+	return (
+		<div>
+			<ExtensionBlankSlate extensionSlug={'etsy'} />
+			{extensionIsReady(
+				isLoadingAgreement,
+				agreement,
+				agreementExtension
+			) && (
+				<>
+					{!agreement?.isCurrentUserAgreementAdmin && (
+						<Container>
+							<Space h={120} />
+							<Center>
+								<Text>
+									Sorry, you do not have permission to view
+									this page. Contact the community owner for
+									help.
+								</Text>
+							</Center>
+						</Container>
+					)}
+
+					{agreement?.isCurrentUserAgreementAdmin && (
+						<div>
+							<ExtensionPageHeader
+								extensionSlug={'etsy'}
+							/>
+
+							<Container>
+								<div>
+									<Text
+										className={meemTheme.tExtraSmallLabel}
+									>
+										{`Link URL`.toUpperCase()}
+									</Text>
+									<Space h={12} />
+									<TextInput
+										radius="lg"
+										size="md"
+										value={linkUrl ?? ''}
+										onChange={(event: {
+											target: {
+												value: React.SetStateAction<string>
+											}
+										}) => {
+											setLinkUrl(event.target.value)
+										}}
+									/>
+									<Space h={40} />
+									<Text
+										className={meemTheme.tExtraSmallLabel}
+									>
+										LINK DISPLAY SETTINGS
+									</Text>
+									<Space h={8} />
+									<div
+										className={meemTheme.spacedRowCentered}
+									>
+										<Switch
+											color={'green'}
+											label={'Display link in sidebar'}
+											checked={shouldDisplayInSidebar}
+											onChange={value => {
+												if (value) {
+													setShouldDisplayInSidebar(
+														value.currentTarget
+															.checked
+													)
+												}
+											}}
+										/>
+									</div>
+									<Space h={16} />
+									<Divider />
+								</div>
+								<div>
+									<Space h={4} />
+									<div
+										className={meemTheme.spacedRowCentered}
+									>
+										<Switch
+											color={'green'}
+											label={
+												'Display link in Favorite Links section'
+											}
+											checked={
+												shouldDisplayInFavoriteLinks
+											}
+											onChange={value => {
+												if (value) {
+													setShouldDisplayInFavoriteLinks(
+														value.currentTarget
+															.checked
+													)
+												}
+											}}
+										/>
+									</div>
+									<Space h={16} />
+									<Divider />
+								</div>
+								<div>
+									<Space h={4} />
+									<div
+										className={meemTheme.spacedRowCentered}
+									>
+										<Switch
+											color={'green'}
+											label={
+												'Hide links if viewer is not a community member'
+											}
+											checked={isPrivateExtension}
+											onChange={value => {
+												if (value) {
+													setIsPrivateExtension(
+														value.currentTarget
+															.checked
+													)
+												}
+											}}
+										/>
+									</div>
+									<Space h={16} />
+									<Divider />
+								</div>
+								<Space h={16} />
+
+								<Button
+									disabled={isDisablingExtension}
+									loading={isDisablingExtension}
+									className={meemTheme.buttonAsh}
+									onClick={disableExtension}
+								>
+									Disable extension
+								</Button>
+
+								<Space h={48} />
+								<Button
+									disabled={isSavingChanges}
+									loading={isSavingChanges}
+									onClick={() => {
+										saveChanges()
+									}}
+									className={meemTheme.buttonBlack}
+								>
+									Save Changes
+								</Button>
+							</Container>
+						</div>
+					)}
+				</>
+			)}
+		</div>
+	)
+}
---
+++ src/pages/[slug]/e/email/index.tsx
@@ -0,0 +1,163 @@
+/* eslint-disable react/prop-types */
+import log from '@kengoldfarb/log'
+import { Space } from '@mantine/core'
+import type { GetServerSideProps, NextPage } from 'next'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import React from 'react'
+import { AgreementProvider } from '../../../../components/AgreementHome/AgreementProvider'
+import { hostnameToChainId } from '../../../../components/App'
+import { EmailLinkExtensionSettings } from '../../../../components/Extensions/EmailLink/EmailLinkExtensionSettings'
+import { MeemFooter } from '../../../../components/Footer/MeemFooter'
+import { HeaderMenu } from '../../../../components/Header/Header'
+import { GET_AGREEMENT_INFO } from '../../../../graphql/agreements'
+import { ssrGraphqlClient } from '../../../../utils/ssr_graphql'
+
+export interface AgreementPropViewModel {
+	responseBody: any
+	description: string
+	isError: boolean
+}
+
+interface IProps {
+	agreement: AgreementPropViewModel
+}
+
+const AgreementExampleExtensionSettingsPage: NextPage<IProps> = ({
+	agreement
+}) => {
+	const router = useRouter()
+
+	const agreementSlug =
+		router.query.slug === undefined ? '' : `${router.query.slug}`
+	return (
+		<>
+			<Head>
+				<title>
+					{agreement === undefined || agreement.isError
+						? 'Not found'
+						: `${agreement.responseBody.Agreements[0].name} | Email Settings | Meem`}
+				</title>
+				<meta
+					name="title"
+					content={
+						agreement === undefined || agreement.isError
+							? 'Not found'
+							: `${agreement.responseBody.Agreements[0].name} | Email Settings | Meem`
+					}
+				/>
+				<meta name="description" content={agreement.description} />
+				<meta property="og:type" content="website" />
+				<meta property="og:url" content="https://app.meem.wtf/" />
+				<meta
+					property="og:title"
+					content={
+						agreement === undefined || agreement.isError
+							? 'Not found'
+							: `${agreement.responseBody.Agreements[0].name} | Email Settings | Meem`
+					}
+				/>
+				<meta
+					property="og:description"
+					content={agreement.description}
+				/>
+				<meta property="twitter:card" content="summary_large_image" />
+				<meta property="twitter:url" content="https://app.meem.wtf/" />
+				<meta
+					property="twitter:title"
+					content={
+						agreement === undefined || agreement.isError
+							? 'Not found'
+							: `${agreement.responseBody.Agreements[0].name} | Email Settings | Meem`
+					}
+				/>
+				<meta
+					name="viewport"
+					content="initial-scale=1, width=device-width"
+				/>
+				<meta property="twitter:image" content="/link-preview.png" />
+				<meta property="og:image" content="/link-preview.png" />
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="/apple-touch-icon.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/favicon32.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/favicon.png"
+				/>
+			</Head>
+			<HeaderMenu />
+			<AgreementProvider slug={agreementSlug}>
+				<EmailLinkExtensionSettings />
+			</AgreementProvider>
+			<Space h={64} />
+			<MeemFooter />
+		</>
+	)
+}
+
+export const getServerSideProps: GetServerSideProps = async ({
+	params,
+	req
+}) => {
+	let agreement: AgreementPropViewModel | undefined
+	const client = ssrGraphqlClient
+
+	try {
+		if (params?.slug) {
+			const { data, errors } = await client.query({
+				query: GET_AGREEMENT_INFO,
+				variables: {
+					slug: params.slug,
+					chainId: hostnameToChainId(req.headers.host ?? '')
+				}
+			})
+
+			if (data.Agreements.length === 0) {
+				agreement = {
+					isError: true,
+					description: 'This community does not exist. Yet.',
+					responseBody: null
+				}
+			} else {
+				agreement = {
+					isError: false,
+					responseBody: data,
+					description: data.Agreements[0].metadata.description ?? ''
+				}
+			}
+			return {
+				props: {
+					agreement,
+					isError: !!errors,
+					description: 'There was an error fetching agreement data'
+				}
+			}
+		}
+
+		return { props: {} }
+	} catch (e) {
+		log.debug(e)
+		agreement = {
+			isError: true,
+			responseBody: null,
+			description: 'This community does not exist. Yet.'
+		}
+		return {
+			props: {
+				agreement
+			}
+		}
+	}
+}
+
+export default AgreementExampleExtensionSettingsPage
