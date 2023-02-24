4ec321ceb6ff0e907d08d1b08f5f4d2b3fa9bc84
James Gadsby <james.gadsby@gmail.com>
2023-02-24 11:58:29 -0500

    feat: much faster page load times by removing GetServerSideProps, update site og description and standardize page title naming convention

--- src/utils/strings.tsx
+++ src/utils/strings.tsx
@@ -3,3 +3,7 @@ export function toTitleCase(str: string) {
 		return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
 	})
 }
+
+export function deslugify(str: string) {
+	return toTitleCase(str.replaceAll('-', ' '))
+}
