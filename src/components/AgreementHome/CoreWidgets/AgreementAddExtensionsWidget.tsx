600d494d09148aa6ae1e28bc37a0362fd3c5f28c
James Gadsby <james.gadsby@gmail.com>
2023-02-24 14:25:26 -0500

    fix: hide 'under construction' message if viewing meem homepage and not a member of the community

--- src/components/AgreementHome/CoreWidgets/AgreementAddExtensionsWidget.tsx
+++ src/components/AgreementHome/CoreWidgets/AgreementAddExtensionsWidget.tsx
@@ -103,31 +103,37 @@ export const AgreementAddExtensionsWidget: React.FC<IProps> = ({
 						</>
 					)}
 
-					{!agreement.isCurrentUserAgreementAdmin && (
-						<>
-							{totalExtensions === 0 && (
-								<div
-									className={meemTheme.widgetExtension}
-									style={{ padding: 32 }}
-								>
-									<Space h={16} />
-									<Center>
-										<Text className={meemTheme.tMediumBold}>
-											Under construction
-										</Text>
-									</Center>
-									<Space h={16} />
-									<Center>
-										<Text className={meemTheme.tSmall}>
-											This community is not using any
-											extensions yet. Check back later!
-										</Text>
-									</Center>
-									<Space h={16} />
-								</div>
-							)}
-						</>
-					)}
+					{!agreement.isCurrentUserAgreementAdmin &&
+						agreement.slug !== 'meem' && (
+							<>
+								{totalExtensions === 0 && (
+									<div
+										className={meemTheme.widgetExtension}
+										style={{ padding: 32 }}
+									>
+										<Space h={16} />
+										<Center>
+											<Text
+												className={
+													meemTheme.tMediumBold
+												}
+											>
+												Under construction
+											</Text>
+										</Center>
+										<Space h={16} />
+										<Center>
+											<Text className={meemTheme.tSmall}>
+												This community is not using any
+												extensions yet. Check back
+												later!
+											</Text>
+										</Center>
+										<Space h={16} />
+									</div>
+								)}
+							</>
+						)}
 				</>
 			)}
 			<AddExtensionModal
