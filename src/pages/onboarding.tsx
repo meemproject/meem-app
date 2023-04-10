04232841ec6f4eb38af8f802ab4375a44507f894
James Gadsby <james.gadsby@gmail.com>
2023-04-10 14:41:54 -0400

    hotfix: auth redirect

--- src/pages/onboarding.tsx
+++ src/pages/onboarding.tsx
@@ -6,7 +6,7 @@ const Home: NextPage = () => {
 	const router = useRouter()
 
 	useEffect(() => {
-		router.push(`/onboard/community-tweets`)
+		router.push(`/onboard/communitytweets`)
 	})
 
 	return <></>
